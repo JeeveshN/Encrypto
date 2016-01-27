68770a7265f6759f710dd6ac1e43abbf9d91a88f
JeeveshN <narang.jeevesh@gmail.com>
2016-01-27 17:25:20 +0530

    One Time Pad Encryption

---
+++ Code/Encryption_Algo.c
@@ -0,0 +1,234 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+char key[5];
+int num(char ch)
+{
+    if(ch<='Z' && ch>='A')
+        return (int)ch-65;
+    else
+        return (int)ch-97;
+}
+char de_num_capital(int key_num)
+{
+	int temp= key_num % 26;
+	temp=temp+65;
+	return (char)temp;
+}
+char de_num_small(int key_num)
+{
+	int temp= key_num % 26;
+	temp=temp+97;
+	return (char)temp;
+}
+char encrypt_algo(char ch)
+{
+    int key_num;
+    static count=0;
+    if((ch<='Z' && ch>='A') || ( ch<='z' && ch>='a'))
+    	{
+    		key_num= num(ch)+num(key[count % 4]);
+    		count++;
+    		if(ch<='Z' && ch>='A')
+                ch=de_num_capital(key_num);
+            else
+                ch=de_num_small(key_num);
+    	}
+    	return ch;
+
+}
+char decrypt_algo(char ch)
+{
+    int key_num;
+    static count=0;
+        if((ch<='Z' && ch>='A') || ( ch<='z' && ch>='a'))
+    	{
+    	    if(num(ch)-num(key[count % 4])>=0)
+    		{
+    		    key_num= num(ch)-num(key[count % 4]);
+                count++;
+                if(ch<='Z' && ch>='A')
+                    ch=de_num_capital(key_num);
+                else
+                    ch=de_num_small(key_num);
+    		}
+    		else
+            {
+                key_num=(26+num(ch))-num(key[count % 4]);
+                count++;
+                if(ch<='Z' && ch>='A')
+                    ch=de_num_capital(key_num);
+                else
+                    ch=de_num_small(key_num);
+            }
+    	}
+    	return ch;
+}
+void encrypt()
+{
+    system("cls");
+    FILE* f1;
+    char m;
+    int count=0,count_num=0;
+    printf("\n\nEnter the 4 letter Key: ");
+    scanf("%c",&m);
+    int i;
+    for(i=0;i<4;i++)
+    {
+        scanf("%c",&key[i]);
+    }
+    key[4]='\0';
+    f1=fopen("Encrypted.txt","w");
+    scanf("%c",&m);
+    char ch;
+    for(i=0;i<4;i++)
+    {
+        fprintf(f1,"%c",encrypt_algo(key[i]));
+    }
+    fprintf(f1," ");
+    printf("Enter the contents:\n");
+    while( (ch=getchar()) != EOF)
+    {
+        ch=encrypt_algo(ch);
+    	putc(ch,f1);
+    }
+    fclose(f1);
+    printf("\n---------------------THE FILE HAS BEEN CREATED--------------------------");
+
+}
+void encrypt_old()
+{
+    system("cls");
+    FILE* f1;
+    FILE* f2;
+    FILE* f3;
+    printf("Select the File");
+    system("File_Chooser.bat");
+    f3=fopen("testlog.txt","r");
+    char File[100],m;
+    int i,count=0,count_num=0;
+    char ch;
+    fscanf(f3,"%[^\n]",File);
+    scanf("%c",&m);
+    system("cls");
+    printf("Enter the 4 letter Key: ");
+    for(i=0;i<4;i++)
+    {
+        scanf("%c",&key[i]);
+    }
+    key[4]='\0';
+    f1=fopen(File,"r");
+    f2=fopen("Enc.txt","w");
+    scanf("%c",&m);
+    count=0;
+    for(i=0;i<4;i++)
+    {
+        fprintf(f2,"%c",encrypt_algo(key[i]));
+    }
+    fprintf(f2," ");
+    while( (ch=getc(f1)) != EOF)
+    {
+    	ch=encrypt_algo(ch);
+    	putc(ch,f2);
+    }
+    fclose(f1);
+    fclose(f2);
+    fclose(f3);
+    remove(File);
+    remove("testlog.txt");
+    rename("Enc.txt",File);
+    printf("\n---------------------THE FILE HAS BEEN ENCRYPTED--------------------------");
+
+}
+void decrypt()
+{
+    system("cls");
+    char File[100],m;
+    FILE* f3;
+    printf("Select the File");
+    system("File_Chooser.bat");
+    f3=fopen("testlog.txt","r");
+    fscanf(f3,"%[^\n]",File);
+    int i,count=0,count_num=0;
+    char ch;
+    scanf("%c",&m);
+    int flag=0;
+    system("cls");
+    FILE* f1;
+    FILE* f2;
+    f1=fopen(File,"r");
+    re_enter:
+    printf("Enter the 4 letter Key: ");
+    i=0;
+    for(i=0;i<4;i++)
+    {
+        scanf("%c",&key[i]);
+    }
+    key[4]='\0';
+
+    for(i=0;i<4;i++)
+    {
+        ch=getc(f1);
+        if(key[i]!=decrypt_algo(ch))
+        {
+            flag++;
+            break;
+        }
+    }
+
+
+    if(flag!=0)
+    {
+        if(flag==3)
+        {
+            fclose(f1);
+            remove(File);
+            printf("----------------------------The File Has Been Deleted---------------------------");
+            return;
+        }
+        else
+        {
+            printf("Invalid Key Only %d try remaining\n",3-flag);
+            goto re_enter;
+        }
+    }
+    ch=getc(f1);
+    f2=fopen("Decrypted.txt","w");
+    while((ch=getc(f1)) != EOF)
+    {
+        ch=decrypt_algo(ch);
+    	putc(ch,f2);
+    }
+    fclose(f1);
+    fclose(f2);
+    fclose(f3);
+    remove(File);
+    remove("testlog.txt");
+    rename("Decrypted.txt",File);
+    printf("\n---------------------THE FILE HAS BEEN DECRYPTED--------------------------");
+}
+
+int main()
+{
+    printf("1. Create an new encrypted File\n");
+    printf("2. Encrypt a file\n");
+    printf("3. Decrypt an encrypted File\n");
+    int choice;
+    printf("Enter your choice : ");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+    case 1:
+        encrypt();
+        getch();
+        return 0;
+    case 2:
+        encrypt_old();
+        getch();
+        return 0;
+    case 3:
+        decrypt();
+        getch();
+        return 0;
+    }
+}
---
+++ Code/File_Chooser.bat
@@ -0,0 +1,42 @@
+:: chooser.bat
+:: launches a File... Open sort of file chooser and outputs choice to the console
+:: http://stackoverflow.com/a/15885133/1683264
+
+@echo off
+setlocal enabledelayedexpansion
+chdir > testlog.txt
+
+:: Does powershell.exe exist within %PATH%?
+for %%I in (`powershell.exe`) do if "%%~$PATH:I" neq "" (
+    set chooser=powershell "Add-Type -AssemblyName System.windows.forms|Out-Null;$f=New-Object System.Windows.Forms.OpenFileDialog;$f.InitialDirectory='%cd%';$f.Filter='Text Files (*.txt)|*.txt|All Files (*.*)|*.*';$f.showHelp=$true;$f.ShowDialog()|Out-Null;$f.FileName"
+) else (
+rem :: If not, compose and link C# application to open file browser dialog
+    set chooser=%temp%\chooser.exe
+    >"%temp%\c.cs" echo using System;using System.Windows.Forms;
+    >>"%temp%\c.cs" echo class dummy{
+    >>"%temp%\c.cs" echo public static void Main^(^){
+    >>"%temp%\c.cs" echo OpenFileDialog f=new OpenFileDialog^(^);
+    >>"%temp%\c.cs" echo f.InitialDirectory=Environment.CurrentDirectory;
+    >>"%temp%\c.cs" echo f.Filter="Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+    >>"%temp%\c.cs" echo f.ShowHelp=true;
+    >>"%temp%\c.cs" echo f.ShowDialog^(^);
+    >>"%temp%\c.cs" echo Console.Write^(f.FileName^);}}
+    for /f "delims=" %%I in ('dir /b /s "%windir%\microsoft.net\*csc.exe"') do (
+        if not exist "!chooser!" "%%I" /nologo /out:"!chooser!" "%temp%\c.cs" 2>NUL
+    )
+    del "%temp%\c.cs"
+    if not exist "!chooser!" (
+        echo Error: Please install .NET 2.0 or newer, or install PowerShell.
+        goto :EOF
+    )
+)
+
+:: capture choice to a variable
+for /f "delims=" %%I in ('%chooser%') do set "filename=%%I"
+
+
+echo %filename% > testlog.txt
+
+:: Clean up the mess
+del "%temp%\chooser.exe" 2>NUL
+goto :EOF
